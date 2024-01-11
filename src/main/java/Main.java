a352d4154cfeb0b56200c93599e7ed5737cb7d36
Mustafa kart <mustafakart000@gmail.com>
2024-01-11 11:45:45 +0300

    3.0

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Surum 1.0");
+        System.out.println("Surum mustafakart1");
     }
+
+    String asd;
 }
