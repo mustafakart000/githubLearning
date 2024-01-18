6673f3d7d9bc59fc499cd7c2a300777d63102a4e
Mustafa kart <mustafakart000@gmail.com>
2024-01-18 03:12:01 +0300

    2

--- src/main/java/IsmailKartt.java
+++ src/main/java/IsmailKartt.java
@@ -2,5 +2,7 @@ public class IsmailKartt {
     public static void main(String[] args) {
         System.out.println("ismail kart 1.01");
         System.out.println("ismail kart 2.1");
+        System.out.println("ismail kart 2.2");
     }
+   
 }
