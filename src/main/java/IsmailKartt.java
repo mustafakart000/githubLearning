6c2beb7440f1c356790e1a0fa6444c839d981d82
Mustafa kart <46137737+mustafakart000@users.noreply.github.com>
2024-01-18 15:27:46 +0300

    Revert "Ismail kart 2.1"

--- src/main/java/IsmailKartt.java
+++ src/main/java/IsmailKartt.java
@@ -2,10 +2,5 @@ public class IsmailKartt {
     public static void main(String[] args) {
         System.out.println("ismail kart 1.01");
         System.out.println("ismail kart 2.1");
-        System.out.println("ismail kart 2.1");
-        System.out.println("ismail kart 2.2");
-        System.out.println("ismail kart 2.3");
-        System.out.println("ismail kart 2.4");
-        System.out.println("ismail kart 2.5");
     }
 }
