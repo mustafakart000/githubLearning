c438b5edacd31b2e4525a1831ead9e3f6d1a4b8b
Mustafa kart <mustafakart000@gmail.com>
2024-01-18 15:35:30 +0300

    1.6 Surum mustafakart000

--- src/main/java/Mustafakart000.java
+++ src/main/java/Mustafakart000.java
@@ -1,9 +1,10 @@
-public class Main {
+public class Mustafakart000 {
     public static void main(String[] args) {
         System.out.println("Surum 1.0");
         System.out.println("1.3 Surum mustafakart000");
         System.out.println("1.4 Surum mustafakart000");
         System.out.println("1.5 Surum mustafakart000");
+        System.out.println("1.6 Surum mustafakart000");
     }
 
     String asd;
