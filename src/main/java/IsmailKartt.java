fd5e4c07284ffc0e1ec5c08edcf3dcdbd82ca7eb
Mustafa kart <mustafakart000@gmail.com>
2024-01-19 15:41:22 +0300

    ismail kart 3.04

--- src/main/java/IsmailKartt.java
+++ src/main/java/IsmailKartt.java
@@ -2,13 +2,10 @@ public class IsmailKartt {
     public static void main(String[] args) {
         System.out.println("ismail kart 1.01");
         System.out.println("ismail kart 2.1");
-
-
         System.out.println("ismail kart 2.2");
         System.out.println("ismail kart 2.3");
         System.out.println("ismail kart 2.4");
         System.out.println("ismail kart 2.5");
-
         System.out.println("ismail kart 2.6");
         System.out.println("ismail kart 2.7");
         System.out.println("ismail kart 2.8");
